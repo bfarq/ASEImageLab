bce415afa13967f08aa71cd2f904d28fb907e321
bfarq <s5077669@bournemouth.ac.uk>
2018-10-12 10:37:12 +0100

    edit5 main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <Magick++.h>
+#include <memory>
 
 int main () 
 {
-    const size_t width=100;
-    const size_t heigt=100;
+    const size_t width=400;
+    const size_t height=400;
     const size_t pixelDepth=3;
-    unsigned char image[width*height*pixelDepth];
+    std::unique_ptr<unsigned char []> image=   std::make_unique<unsigned char[]> (width*height*pixelDepth );
 
     for(size_t i=0; i<width*height*pixelDepth; ++i)
     {
-        image[i]=255;
+        image[i]=128;
     }
-    for(size_t i=0; i<width*height*pixelDepth; ++i)
-    {
-        std::cout<<image[i]<<' ';
-    }
-    
+
+    Magick::Image output(width,height,"RGB",Magick::CharPixel,image.get());
+    output.write("test.bmp");
     return EXIT_SUCCESS;
 
 }
